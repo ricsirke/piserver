c4f42ad994f0f9c29a9e51f2261eba72f0c8d779
ricsirke <benericsirke@gmail.com>
2017-01-22 03:25:36 +0100

    dbg

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ dc = 0
 p.start(dc)
 
 thrStrob = thr.Event()
+th = thr.Thread()
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ def doTask(task, *args):
     
     try:
         thrStrob.set()
+        
     except:
         pass
         
@@ -52,6 +54,7 @@ def doSetLum(incr):
     
 def doStrob(spd):
     global thrStrob
+    global th
     
     t = spd/float(100)
     print "doStrob", spd, t
@@ -66,7 +69,9 @@ def doStrob(spd):
             time.sleep(t)
     
     thrStrob.clear()
-    th = thr.Thread(target=loop, args=(p, thrStrob, t))
+    
+    th.target = loop
+    th.args = args=(p, thrStrob, t)
     th.start()
     
     
