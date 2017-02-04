e4c5b6d2dacbcb4b1e1d7ea769d02d9e4b1aed2d
ricsirke <benericsirke@gmail.com>
2017-02-04 12:01:13 +0100

    temphum-chart

--- static/index.js
+++ static/index.js
@@ -86,7 +86,49 @@ $(function(){
         doTempHumXhr();
     };
     
+    var data = [
+        {"hum": 22.5, "t": "2017-02-04 11:30:01.554436", "temp": 22.5},
+        {"hum": 22.5, "t": "2017-02-04 12:00:01.554436", "temp": 22.0},
+        {"hum": 22.5, "t": "2017-02-04 12:30:01.554436", "temp": 22.0},
+        {"hum": 22.5, "t": "2017-02-04 13:00:01.554436", "temp": 22.5},
+        {"hum": 22.5, "t": "2017-02-04 13:30:01.554436", "temp": 23.0},
+        {"hum": 22.5, "t": "2017-02-04 14:00:01.554436", "temp": 23.0},
+        {"hum": 22.5, "t": "2017-02-04 14:30:01.554436", "temp": 23.0},
+        {"hum": 22.5, "t": "2017-02-04 15:00:01.554436", "temp": 23.5},
+        {"hum": 22.5, "t": "2017-02-04 15:30:01.554436", "temp": 23.5},
+        {"hum": 22.5, "t": "2017-02-04 16:00:01.554436", "temp": 24.0},
+    ];
     
+    var parseTime = d3.timeParse("%d-%b-%y" "%y-%b-%d %h:%m:%s.%ms");
+    
+    data = data.map(function(d){
+        d["t"] = parseTime(d["t"]);
+        
+        return d;
+    })
+    
+    var svg = d3.select("svg"),
+        g = svg.append("g"),
+        width = svg.attr("width"),
+        height = svg.attr("height"),
+        x_ax = d3.scaleTime()
+                 .rangeRound([0, width])
+                 .domain(d3.extent(data, function(d){ return d["t"]; }),
+        y_ax = d3.scaleLinear()
+                 .rangeRound([0, height])
+                 .domain(d3.extent(data, function(d){ return d["temp"] }),
+        lineGen = d3.line()
+                    .x(function(d){ return d["t"]; })
+                    .y(function(d){ return d["temp"]; });
+              
+        g.append("path")
+         .datum(data)
+         .attr("fill", "none")
+         .attr("stroke", "steelblue")
+         .attr("stroke-linejoin", "round")
+         .attr("stroke-linecap", "round")
+         .attr("stroke-width", 1.5)
+         .attr("d", lineGen);
 });
 
 
